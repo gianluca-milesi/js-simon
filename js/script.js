5424ee7be967fae6e8c3cd109400f799bbb3dc72
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-24 18:23:53 +0200

    ADD: button event listener function to check, push and show messages of correct numbers array

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@ const listNumbers = document.getElementById("numbers-list");
 const countdown = document.getElementById("countdown");
 const instructions = document.getElementById("instructions");
 const answersForm = document.getElementById("answers-form");
+const checkButton = document.querySelector("button.btn");
+const inputsGroup = document.getElementById("input-group");
+const inputs = inputsGroup.querySelectorAll("input");
+const message = document.getElementById("message");
 
 let randomNumbers = [];
-
-
-
-
-
 
 
 function generateRandomNumbers() {
@@ -57,3 +56,28 @@ function startCountdown() {
 generateRandomNumbers();
 viewNumbers();
 startCountdown();
+
+
+checkButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const userNumbers = [];
+    for (let i = 0; i < inputs.length; i++) {
+        userNumbers.push(parseInt(inputs[i].value));
+    }
+    console.log(userNumbers);
+
+    const correctNumbers = [];
+    for (let i = 0; i < randomNumbers.length; i++) {
+        if (userNumbers.includes(randomNumbers[i])) {
+            correctNumbers.push(randomNumbers[i]);
+        }
+    }
+    console.log(correctNumbers);
+
+    if (correctNumbers.length > 0) {
+        message.innerHTML = `Hai indovinato i seguenti numeri: ${correctNumbers.join(", ")}`;
+    } else {
+        message.innerHTML = `Non hai indovinato nessun numero`;
+    }
+});
